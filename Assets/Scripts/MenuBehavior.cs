133a3e79bd5bd0c9e317dd3d99e27671d4de82ec
Catz-Gamez <andreas.moosbauer3118@gmail.com>
2023-08-08 15:42:30 +0200

    'Main Menu'
    - added Main Menu
    - added Menu Behaviour
    - added Icon Sprites

---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehavior : MonoBehaviour {
+    public void ExitGame() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+    public void LoadScene(int sceneNumber) {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
